d9808d5fc6e77caa5547213491e52f7c9ed7f760
Ahmad Fauzi Abdilah <102431802+AhmadFauziAbdillah@users.noreply.github.com>
2025-06-28 02:24:43 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -6,13 +6,11 @@ $user = 'ebb596d777bf8000624be6a0442e';
 $password = '0685ebb5-96d7-7916-8000-7c8ac6004371';
 $database = 'dbscan'; // You shouldn't use the "root" database. This is just for the example. The recommended way is to create a dedicated database (and user) in PhpMyAdmin and use it then here.
 
-$dsn = "mysql:host=$hostname;port=$port;dbname=$database";
+$dsn = "mysql:host=$hostname;port=$port;dbname=$database;charset=utf8mb4";
 
 $options = array(
-  // See below if you have an error like "Uncaught PDOException: PDO::__construct(): SSL operation failed with code 1. OpenSSL Error messages: error:0A000086:SSL routines::certificate verify failed".
-  PDO::MYSQL_ATTR_SSL_CAPATH => '/etc/ssl/certs/',
-  // PDO::MYSQL_ATTR_SSL_CA => 'isrgrootx1.pem',
-  PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => true,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 );
 
 $pdo = new PDO($dsn, $user, $password, $options);
